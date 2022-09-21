21ea627b9f2559a8626686bd3f453680f6e21ada
kevinmarcellius <kevin.marcellius@ui.ac.id>
2022-09-21 09:32:16 +0700

    update models.py

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class WatchlistItem(models.Model):
-    watched = models.CharField(max_length=5)
+    watched = models.BooleanField()
     title = models.CharField(max_length=50)
     rating = models.FloatField()
     release_date = models.CharField(max_length=20)
